f7b9a4876847ecdc7513d36f398fd4fdf146fe9f
Obi1kan <danielgribinet1234@gmail.com>
2023-04-04 13:52:21 +0300

    create .env.example file

--- packages/user-interface/src/App.tsx
+++ packages/user-interface/src/App.tsx
@@ -3,6 +3,7 @@ import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Homepage } from './pages/Homepage';
 import { Routes, Route } from 'react-router-dom';
+
 import { UserContext } from './context/user-context';
 import { UserPosts } from './pages/UserPosts';
 import { Users } from './pages/Users';
@@ -13,16 +14,18 @@ export function App() {
     <>
       {isAuth ? (
         <Routes>
-          <Route path='/' element={<Homepage />} />
+          <Route path='' element={<Homepage />} />
           <Route path='/own-posts' element={<UserPosts />} />
           {user?.permission === 'admin' && (
             <Route path='/users' element={<Users />} />
           )}
+          <Route path='*' element={<Navigate to='' />} />
         </Routes>
       ) : (
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='*' element={<Navigate to='/login' />} />
         </Routes>
       )}
     </>
