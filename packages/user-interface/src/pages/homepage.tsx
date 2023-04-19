8abb641c542978d63591f389c81de63452c683f0
Obi1kan <danielgribinet1234@gmail.com>
2023-04-19 10:19:36 +0300

    Setup lint-staged to project

--- packages/user-interface/src/pages/homepage.tsx
+++ packages/user-interface/src/pages/homepage.tsx
@@ -1,32 +1,35 @@
-import { Post } from '../types';
-import { Header } from '../components/Header';
-import { PostCard } from '../components/PostCard';
-import { useQuery } from 'react-query';
-import { mainAxios } from '../utils';
-import { Loading } from '../components/Loading';
-import { ErrorPage } from '../components/ErrorPage/ErrorPage';
+import { Post } from "../types";
+import { Header } from "../components/Header";
+import { PostCard } from "../components/PostCard";
+import { useQuery } from "react-query";
+import { mainAxios } from "../utils";
+import { Loading } from "../components/Loading";
+import { ErrorPage } from "../components/ErrorPage/ErrorPage";
 export function Homepage() {
-  const { data, isLoading, error } = useQuery('posts', () => {
-    return mainAxios.get('/posts');
-  });
+    const { data, isLoading, error } = useQuery("posts", () => {
+        return mainAxios.get("/posts");
+    });
 
-  consolen.log(data?.data);
-  return (
-    <>
-      {isLoading && <Loading />}
-      {error && <ErrorPage />}
-      {data && (
-        <div className='content'>
-          <Header />
-          <div className='home-page'>
-            <div className='row justify-content-center' style={{ gap: '80px' }}>
-              {data?.data.map((post: Post) => (
-                <PostCard {...post} />
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+    consol.log(data?.data);
+    return (
+        <>
+            {isLoading && <Loading />}
+            {error && <ErrorPage />}
+            {data && (
+                <div className="content">
+                    <Header />
+                    <div className="home-page">
+                        <div
+                            className="row justify-content-center"
+                            style={{ gap: "80px" }}
+                        >
+                            {data?.data.map((post: Post) => (
+                                <PostCard {...post} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
